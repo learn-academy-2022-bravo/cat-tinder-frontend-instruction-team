02248fd3157dba4a2e255cf29c3f0185882921f9
Elyse Montano <elysemontano.dev.com>
2022-05-11 12:36:48 -0700

    adds read functionality

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 class CatIndex extends Component {
   render() {
     return(
       <>
-        <h2>CatIndex</h2>
+        <h2>Cat Index</h2>
+        {this.props.cats && this.props.cats.map(cat => {
+          return <NavLink to={`/catshow/${cat.id}`} key={cat.id}>{cat.name}</NavLink>
+        })}
       </>
     )
   }
